351e8e51e6308dad70734e65d3b67c3071c77796
Alex Taxiera <alex.taxiera@gmail.com>
2018-02-06 22:07:03 -0500

    clean and prettify

--- readHDF5/read.py
+++ readHDF5/read.py
@@ -3,24 +3,24 @@ import h5py
 import json
 
 f = h5py.File(sys.argv[1], 'r')
-d = {}
+d = {'id': sys.argv[1][2:-3]}
 for i in f.keys():
     d[i] = {}
     for j in f[i].keys():
         x = list()
         for k in range(len(f[i][j])):
             y = f[i][j][k]
+            if y.__class__.__name__ == 'int32':
+                y = str(y)
             if y.__class__.__name__ == 'void':
                 z = [ a for a in y]
                 if i == 'metadata':
                     j = 'song'
-                    z = {'artist': z[9].decode('UTF-8'), 'song_title': z[18].decode('UTF-8')}
+                    x = {'artist': z[9].decode('UTF-8'), 'title': z[18].decode('UTF-8')}
                 elif i == 'musicbrainz':
                     j = 'year'
-                    z = str(z[1])
-                else:
-                    continue
-                y = z
+                    x = str(z[1])
+                continue
             elif y.__class__.__name__ == 'bytes_':
                 y = y.decode('UTF-8')
             elif y.__class__.__name__ == 'ndarray':
