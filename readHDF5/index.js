749ca7c5a13d2a24596e2d7ac0c4d5a153fab6a5
Alex Taxiera <alex.taxiera@gmail.com>
2018-02-06 22:07:15 -0500

    handle errors

--- readHDF5/index.js
+++ readHDF5/index.js
@@ -1,3 +1,9 @@
 module.exports = function (filepath) {
-  return JSON.parse((require('child_process').spawnSync('python', [require('path').join(__dirname, '/read.py'), filepath])).stdout)
+  const output = require('child_process').spawnSync('python', [require('path').join(__dirname, '/read.py'), filepath])
+  if (`${output.stderr}`) {
+    console.log(`${output.stderr}`)
+    console.log(filepath)
+  } else {
+    return JSON.parse(output.stdout)
+  }
 }
