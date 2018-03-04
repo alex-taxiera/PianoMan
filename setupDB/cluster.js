a63ebab7e1160422738247371bfe5c45caaef0d8
Dozar <ddozar@gmail.com>
2018-03-04 11:58:17 -0800

    Updated for Kmeans implementation & DB print

--- setupDB/cluster.js
+++ setupDB/cluster.js
@@ -14,68 +14,84 @@ var md = require('knex')({
   pool: { min: 1, max: 100 }
 })
 
-//To remove = duration, hotttnesss, danceability, energy, start_of_fade_out,
-//end_of_fade_in
+// To remove = duration, hotttnesss, danceability, energy, start_of_fade_out,
+// end_of_fade_in
 
 // X = key (0-11), sections_start,
 // Y = mode (0 or 1), tempo, segments_start, segments_loud
 
-function normalizeFloat(value, max, min) {
+function normalizeFloat (value, max, min) {
   return ((value - min) / (max - min))
 }
 
-async function tempoMaxFind() {
+async function tempoMaxFind () {
   return md('metadata').max('tempo').then((maxboi) => {
-    tempoMax = maxboi[0]['max(`tempo`)']
+    let tempoMax = maxboi[0]['max(`tempo`)']
     return tempoMax
   })
 }
 
-async function tempoMinFind() {
+async function tempoMinFind () {
   return md('metadata').min('tempo').then((minboi) => {
     let tempoMin = minboi[0]['min(`tempo`)']
     return tempoMin
   })
 }
-function loudMaxFind() {
-  md('metadata').max('loudness').then((maxboi) => {
-    let tempoMax = maxboi[0]['max(`loudness`)']
-    return loudMax
-  })
-}
+// function loudMaxFind () {
+//   md('metadata').max('loudness').then((maxboi) => {
+//     let loudMax = maxboi[0]['max(`loudness`)']
+//     return loudMax
+//   })
+// }
+//
+// function loudMinFind () {
+//   md('metadata').min('loudness').then((minboi) => {
+//     let loudMin = minboi[0]['min(`loudness`)']
+//     return loudMin
+//   })
+// }
 
-function loudMinFind() {
-  md('metadata').min('loudness').then((minboi) => {
-    let loudMin = minboi[0]['min(`loudness`)']
-    return loudMin
-  })
-}
-
-let graphArray = new Array()
+let graphArray = []
+let resultDict = {}
 
 md('metadata').select('*').then(async (rows) => {
   let arraylen = rows.length
   for (let i = 0; i < arraylen; i++) {
+    let xyPair = []
     let song = rows[i]
     let pairX = 0
     let pairY = 0
     let tempoMax = await tempoMaxFind()
     let tempoMin = await tempoMinFind()
 
-    pairX = (normalizeFloat(song.tempo, tempoMax, tempoMin)) //+ normalizeFloat(song.loudness)) / 2
-    pairY = normalizeFloat(song.key, 11, 0) //Key is always 1-12
+    pairX = (normalizeFloat(song.tempo, tempoMax, tempoMin)) // + normalizeFloat(song.loudness)) / 2
+    pairY = normalizeFloat(song.key, 11, 0) // Key is always 1-12
+    xyPair = [pairX, pairY]
+    resultDict[JSON.stringify(xyPair)] = song.track_id
 
     graphArray.push([pairX, pairY])
-    //let sections = JSON.parse(song.sections_start)
-    //let sectionavg = sections.reduce((a,b) => a + b, 0) / song.sections_start.length
+    // let sections = JSON.parse(song.sections_start)
+    // let sectionavg = sections.reduce((a,b) => a + b, 0) / song.sections_start.length
   }
 
-  var cluster = require('k-means');
+  var clusterMaker = require('clusters')
 
-  cluster (graphArray, { clusters: 15, iterations: 10 }, (results) => {
-    console.log(results)
+  clusterMaker.k(15)
+  clusterMaker.iterations(850)
+  clusterMaker.data(graphArray)
+
+  let centroids = clusterMaker.clusters()
+
+  let final = centroids.map((_, i) => {
+    return { centroid: i, songs: _.points.map((point) => resultDict[JSON.stringify(point)]) }
   })
+  console.log(final)
 
+  for (let centroid of final) {
+    for (let song of centroid.songs) {
+      md('songs').where({track_id: song}).update({clusters: centroid.centroid}).then(console.log)
+    }
+  }
 })
 
 // md('metadata').select('*').where(`track_id=${song1.track_id}`).then((newrows) => {
