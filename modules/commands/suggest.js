01877e9dc2106139d776784e97bf561610a3334f
Dozar <ddozar@gmail.com>
2018-03-20 17:50:25 -0700

    suggestions implementation

--- modules/commands/suggest.js
+++ modules/commands/suggest.js
@@ -2,6 +2,7 @@ const Command = require('../classes/Command.js')
 const Response = require('../classes/Response.js')
 const moment = require('moment')
 const wiki = requre('wiki').default
+const db = require('../database.js')
 
 module.exports = new Command(
   'suggest',
@@ -12,7 +13,7 @@ module.exports = new Command(
     // get data on message.member.id
     // call algorithm on user database
     // simulated user data
-    let info = { id: 'abc', recent_songs: []}
+    let info = { id: 'abc', recent_songs: ['SOHXFBA12A8C13D637', 'SOLGHDZ12AB0183B11', 'SOLJCCO12A6701F987', 'SOMZZON12A6701D3B9', 'SONHOTT12A8C13493C']}
     let suggestions = [
       {name: 'All Star', sLink: 'https://www.youtube.com/watch?v=L_jWHffIx5E', artist: 'Smash Mouth', aLink: 'https://en.wikipedia.org/wiki/Smash_Mouth'},
       {name: 'Photograph', sLink: 'https://www.youtube.com/watch?v=BB0DU4DoPP4', artist: 'Nickelback', aLink: 'https://en.wikipedia.org/wiki/Nickelback'},
@@ -62,4 +63,21 @@ module.exports = new Command(
 
 async function wiki (term) {
   return (await wiki().page(term)).raw.fullurl
+}
+
+async function returnSuggest (data) {
+  // Array of tuples [cluster, location]
+  let temp = data.recent_songs.map((val) => { return { table: 'songs', columns: ['song_id', 'cluster', 'location'], where: { song_id: val } } })
+  let rows = await Promise.all(temp.map(db.select))
+  let values = []
+  for (let i = 0; rows.length > i; i++) {
+    values.push(rows[i][0].cluster)
+  }
+  let mode = math.mode(values)
+
+  let suggests = await db.select({ table: 'songs', columns: ['artist_id', 'title', 'album'], where: { cluster: mode }, limit: 5 })
+  for (let i = 0; i < suggests.length; i++) {
+    suggests[i].artist = (await db.select({ table: 'artists', columns: ['name'], where: { artist_id: suggests[i].artist_id } }))[0].name
+  }
+  return suggests
 }
